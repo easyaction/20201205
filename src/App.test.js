8a00ae0197677859b4f6960844e267519f9ec057
seyeong.kim <seyeong.kim@linecorp.com>
2020-10-25 22:03:49 +0900

    init commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/부부의 탄생/i);
   expect(linkElement).toBeInTheDocument();
 });
