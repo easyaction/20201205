49abac179ff860b0da6d79b42fee4993cb6e37a0
seyeong.kim <seyeong.kim@linecorp.com>
2020-10-25 22:04:44 +0900

    Add uncommitted files

---
+++ src/component/title/Title.jsx
@@ -0,0 +1,14 @@
+import { Component, Fragment } from "react";
+import "./Title.css";
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="background">
+		<h1 className="title">부부의<br/>탄생</h1>
+      </div>
+    );
+  }
+}
+
+export default Title;
