7955e87a18833823da4b075f5921e83fcd4278cd
seyeong-kim <seyeong.kim@linecorp.com>
2020-11-03 23:08:02 +0900

    Add gallery detail

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import "./App.css";
-import { Component } from "react";
-import { Helmet } from "react-helmet";
+import React, {Component} from "react";
+import {Helmet} from "react-helmet";
 import Title from "./component/title/Title";
 import Contents from "./component/contents/Contents";
+import Gallery from './component/gallery/Gallery';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {isGallery: false};
+  }
+
   render() {
     return (
       <div className="App">
         <Helmet>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1"
-          ></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Helmet>
-        <Title />
-        <Contents />
+        <div>
+          <Title />
+          <Contents />
+          {/*<Gallery/>*/}
+        </div>
       </div>
     );
   }
---
+++ src/component/gallery/Gallery.jsx
@@ -0,0 +1,41 @@
+import {Component} from "react";
+import "./Gallery.css";
+import {ReactComponent as IconDelete} from '../../img/btn_delete.svg';
+import Image1 from '../../img/gallery/img_03_gallery_03_detail_01@2x.png';
+import Image2 from '../../img/gallery/img_03_gallery_03_detail_02@2x.png';
+import Image3 from '../../img/gallery/img_03_gallery_03_detail_03@2x.png';
+import Image4 from '../../img/gallery/img_03_gallery_03_detail_04@2x.png';
+import Image5 from '../../img/gallery/img_03_gallery_03_detail_05@2x.png';
+import Image6 from '../../img/gallery/img_03_gallery_03_detail_06@2x.png';
+import Image7 from '../../img/gallery/img_03_gallery_03_detail_07@2x.png';
+import Image8 from '../../img/gallery/img_03_gallery_03_detail_08@2x.png';
+
+
+class Gallery extends Component {
+  render() {
+  return (
+    <div className="gallery-detail">
+      <div className="head">
+        <div className="title">
+          <p>사진첩</p>
+        </div>
+        <div className="icon-delete">
+          <IconDelete/>
+        </div>
+      </div>
+      <div className="body">
+        <img src={Image1} alt="" />
+        <img src={Image2} alt="" />
+        <img src={Image3} alt="" />
+        <img src={Image4} alt="" />
+        <img src={Image5} alt="" />
+        <img src={Image6} alt="" />
+        <img src={Image7} alt="" />
+        <img src={Image8} alt="" />
+      </div>
+    </div>
+  )
+  }
+}
+
+export default Gallery;
